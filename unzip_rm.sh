d56b48eaa7e8391e6731b31ba107ee7fe96705c0
SamSamhuns <sthapuskar@yahoo.com>
2018-12-20 14:33:38 -0500

    Edited the _MACOSX folder name

---
+++ unzip_rm.sh
@@ -0,0 +1,13 @@
+unzip aisles.csv.zip
+unzip departments.csv.zip
+unzip orders.csv.zip
+unzip products.csv.zip
+unzip order_products__prior.csv.zip
+unzip order_products__train.csv.zip
+unzip sample_submission.csv.zip
+
+rm -f *.zip
+rm -rf _MACOSX
+
+mkdir csv_src
+mv *.csv csv_src/
